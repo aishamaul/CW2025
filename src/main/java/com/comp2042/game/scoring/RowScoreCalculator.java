f489c3343c52c6217a7b75ec65258c730a0a9988
aishamaul <aishaamaul@gmail.com>
2025-12-06 23:36:16 +0800

    Refactor: Replace magic number 50 with BASE_SCORE_MULTIPLIER constant

--- src/main/java/com/comp2042/game/scoring/RowScoreCalculator.java
+++ src/main/java/com/comp2042/game/scoring/RowScoreCalculator.java
@@ -1,7 +1,7 @@
 package com.comp2042.game.scoring;
 
 public class RowScoreCalculator {
+    private static final int BASE_SCORE_MULTIPLIER = 50;
     public int calculateScoreBonus(int linesRemoved){
-        return 50 * linesRemoved * linesRemoved;
-    }
+        return BASE_SCORE_MULTIPLIER * linesRemoved * linesRemoved;    }
 }
