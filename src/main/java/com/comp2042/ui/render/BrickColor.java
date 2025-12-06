1e03d6cd7537bf7845e78bdeb64de7ec0f23a869
aishamaul <aishaamaul@gmail.com>
2025-12-06 18:04:01 +0800

    Refactor: remove unused getBaseColor method.

--- src/main/java/com/comp2042/ui/render/BrickColor.java
+++ src/main/java/com/comp2042/ui/render/BrickColor.java
@@ -6,32 +6,6 @@ import javafx.scene.shape.Rectangle;
 
 public class BrickColor {
 
-    public Color getBaseColor(int i) {
-        Paint returnPaint;
-
-        switch (i) {
-            case 1:
-                return Color.ORANGE;
-            case 2:
-                return Color.ORANGERED;
-            case 3:
-                return Color.YELLOW;
-            case 4:
-                return Color.GREENYELLOW;
-            case 5:
-                return Color.AQUA;
-            case 6:
-                return Color.BLUEVIOLET;
-            case 7:
-                return Color.DEEPPINK;
-            case 8:
-                return Color.GREY;
-            case 9:
-                return Color.RED;
-            default:
-                return Color.WHITE;
-        }
-    }
 
     public void applyBrickStyle(Rectangle rectangle, int colorCode) {
         rectangle.getStyleClass().removeIf(name -> name.startsWith("brick-") || name.equals("brick"));
